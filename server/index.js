6f2126441ac820d4fea5804a7ee7df78606fb2e8
ali <alinosratipour@gmail.com>
2021-10-06 00:28:14 +0100

    change env file

--- server/index.js
+++ server/index.js
@@ -84,7 +84,7 @@ app.delete("/todos/:id" , async (req, res)=>{
     }
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>{
     console.log("server started");
 })
