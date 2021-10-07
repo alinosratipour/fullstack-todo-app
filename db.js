40b90347fbbe415769b00a6dcded6c40ce15e206
ali <alinosratipour@gmail.com>
2021-10-07 13:43:19 +0100

    fixed some deployment issues

--- db.js
+++ db.js
@@ -15,16 +15,22 @@ require("dotenv").config();
 // const pool = new Pool(process.env.NODE_ENV === "production" ? proConfig : devConfig);
 
 
- const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.
-PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
+//  const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.
+// PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
 
-const proConfig = process.env.DATABASE_URL //heroku addon   
-const pool = new Pool({
-  connectionString:   process.env.NODE_ENV === "production" ? proConfig : devConfig,     
-   });
+// const proConfig = process.env.DATABASE_URL //heroku addon   
+// const pool = new Pool({
+//   connectionString:   process.env.NODE_ENV === "production" ? proConfig : devConfig,     
+//    });
 
-
-        
+const isProduction = process.env.NODE_ENV === "production";
+const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+   const pool = new Pool({
+    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});     
    
 
 module.exports = pool;
